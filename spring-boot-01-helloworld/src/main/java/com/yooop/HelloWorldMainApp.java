9797528bcfbd1c233be14ba9a7eac25b204c385b
yashupian <306905198@qq.com>
2020-04-17 23:52:50 +0800

    add spring boot 'helloworld' demo code

---
+++ spring-boot-01-helloworld/src/main/java/com/yooop/HelloWorldMainApp.java
@@ -0,0 +1,13 @@
+package com.yooop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApp {
+    public static void main(String[] args) {
+
+        //run the app
+        SpringApplication.run(HelloWorldMainApp.class, args);
+    }
+}
